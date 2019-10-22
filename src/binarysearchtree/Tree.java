5d8527ca1959acbdde7fd31d6ca20d3fc6245ae2
DAVID <dpunzel15@georgefox.edu>
2019-10-22 09:32:55 -0700

    Initial commit

---
+++ src/binarysearchtree/Tree.java
@@ -0,0 +1,9 @@
+package binarysearchtree;
+
+public interface Tree<T> {
+    public void traversal();
+    public void insert(T data);
+    public void delete(T data);
+    public T getMaxValue();
+    public T getMinValue();
+}
