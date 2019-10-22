24d67ab71d6b3fe47f840f4ef3357ffb826323ff
DAVID <dpunzel15@georgefox.edu>
2019-10-22 13:18:43 -0700

    delete method, debug removing item with 2 children not working properly.

--- src/binarysearchtree/App.java
+++ src/binarysearchtree/App.java
@@ -2,17 +2,15 @@ package binarysearchtree;
 
 public class App {
     public static void main(String[] args) {
-        Tree<String> bst = new BinarySearchTree<>();
+        Tree<Integer> bst = new BinarySearchTree<>();
 
-        bst.insert("Zany");
-        bst.insert("Tommy");
-        bst.insert("Madden");
-        bst.insert("Cory");
-        bst.insert("Hector");
-        bst.insert("Adam");
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+        bst.insert(3);
+        bst.insert(7);
 
-//        System.out.println(bst.getMaxValue());
-//        System.out.println(bst.getMinValue());
+        bst.delete(10);
         bst.traversal();
 
     }
--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -47,14 +47,52 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
     private Node<T> delete(Node<T> node, T data) {
 
         // if node is null return node
+
         if (node == null) return node;
 
         // if the given data to be removed is smaller
         // than the node we want to remove, consider the left sub tree
         if (data.compareTo(node.getData()) < 0 ) {
             node.setLeftChild(delete(node.getLeftChild(), data));
+        } else if (data.compareTo(node.getData()) < 0){
+            node.setRightChild(delete(node.getRightChild(), data));
+        } else {
+            // we have found the node we want to remove
+            // this is where we check if given node is a leaf node
+            if (node.getLeftChild() == null && node.getRightChild() == null) {
+                System.out.println("Removing a leaf node... ");
+                return null;
+            }
+
+            // Here we know it has either left child or right child
+            if (node.getLeftChild() == null) {
+                System.out.println("Removing the right child...");
+                Node<T> tempNode = node.getRightChild();
+                node = null;
+                return tempNode;
+            } else if (node.getRightChild() == null) {
+                System.out.println("Removing the left child...");
+                Node<T> tempNode = node.getLeftChild();
+                node = null;
+                return tempNode;
+            }
+
+            // This is the node with two children
+            System.out.println("Removing item with two children.... ");
+            Node<T> tempNode = getPredecessor(node.getLeftChild());
+
+            node.setData(tempNode.getData());
+            node.setLeftChild(delete(node.getLeftChild(), tempNode.getData()));
+
         }
-        return null;
+        return node;
+    }
+
+    private Node<T> getPredecessor(Node<T> node) {
+        if (node.getRightChild() != null)
+            return  getPredecessor(node.getRightChild());
+
+        return node;
     }
 
     private void insertNode(T newData, Node<T> node) {
