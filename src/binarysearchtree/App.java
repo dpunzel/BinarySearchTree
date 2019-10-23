c6c1e576cc4de2755935ba929d316971885dd8ad
DAVID <dpunzel15@georgefox.edu>
2019-10-22 21:36:43 -0700

    Added Person class, more testing

--- src/binarysearchtree/App.java
+++ src/binarysearchtree/App.java
@@ -2,16 +2,26 @@ package binarysearchtree;
 
 public class App {
     public static void main(String[] args) {
-        Tree<Integer> bst = new BinarySearchTree<>();
 
-        bst.insert(10);
-        bst.insert(5);
-        bst.insert(15);
-        bst.insert(3);
-        bst.insert(7);
+        Tree<Person> bst = new BinarySearchTree<>();
 
-        bst.delete(3);
+        bst.insert(new Person("Adam", 28));
+        bst.insert(new Person("Peter", 12));
+        bst.insert(new Person("Kelly", 42));
+        bst.insert(new Person("Toby", 62));
+        bst.insert(new Person("Bob", 32));
+
         bst.traversal();
+//        Tree<Integer> bst = new BinarySearchTree<>();
+//
+//        bst.insert(10);
+//        bst.insert(5);
+//        bst.insert(15);
+//        bst.insert(3);
+//        bst.insert(7);
+//
+//        bst.delete(15);
+//        bst.traversal();
 
     }
 }
