034dcca84a3190a898916ff6fbdf089d1a1cefec
DAVID <dpunzel15@georgefox.edu>
2019-10-24 22:11:31 -0700

    TreeCompareHelper - tested

--- src/binarysearchtree/App.java
+++ src/binarysearchtree/App.java
@@ -3,15 +3,35 @@ package binarysearchtree;
 public class App {
     public static void main(String[] args) {
 
-        Tree<Person> bst = new BinarySearchTree<>();
+        Tree<Integer> bst = new BinarySearchTree<>();
 
-        bst.insert(new Person("Adam", 28));
-        bst.insert(new Person("Peter", 12));
-        bst.insert(new Person("Kelly", 42));
-        bst.insert(new Person("Toby", 62));
-        bst.insert(new Person("Bob", 32));
+        bst.insert(2);
+        bst.insert(22);
+        bst.insert(222);
+        bst.insert(2222);
+        bst.insert(22222);
 
-        bst.traversal();
+        Tree<Integer> bst2 = new BinarySearchTree<>();
+
+        bst2.insert(2);
+        bst2.insert(21);
+        bst2.insert(222);
+        bst2.insert(2222);
+        bst2.insert(22222);
+
+        TreeCompareHelper<Integer> helper = new TreeCompareHelper<>();
+
+        System.out.println(helper.compareTrees(bst.getRoot(), bst2.getRoot()));
+
+//        Tree<Person> bst = new BinarySearchTree<>();
+//
+//        bst.insert(new Person("Adam", 28));
+//        bst.insert(new Person("Peter", 12));
+//        bst.insert(new Person("Kelly", 42));
+//        bst.insert(new Person("Toby", 62));
+//        bst.insert(new Person("Bob", 32));
+//
+//        bst.traversal();
 //        Tree<Integer> bst = new BinarySearchTree<>();
 //
 //        bst.insert(10);
